6b8c1bc323c78f90b0c2133975b70a880e6ad767
Jessy Franco <jesicafranco_1518@outlook.es>
2025-05-22 23:02:54 -0300

    socketCliente

---
+++ socketCliente.py
@@ -0,0 +1,17 @@
+import socket
+socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_cliente.connect(("127.0.0.1", 7777))
+
+while True:
+    mensaje = str(input(">> "))
+    if mensaje.lower() == "salir":
+        break 
+
+    socket_cliente.send(mensaje.encode("utf-8"))
+
+    recibido = socket_cliente.recv(1024)
+    print ("Recibido: ", recibido)
+    
+
+print("adios")
+socket_cliente.close()
